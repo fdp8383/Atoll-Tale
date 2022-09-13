3ee056ac7a289307e56c10bafbf228ecd6713435
Zaven Kazandjian <dinowolf10@gmail.com>
2022-09-13 00:11:42 -0400

    Added first pass of player digging mechanic
    - Player can dig ground tiles directly underneath them
    - Player input and movement is restricted during dig animation
    - Player can only dig on ground tiles once
    - Added placeholder ground materials and placeholder time for dig animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     private CharacterController playerController;
 
     [SerializeField]
+    private PlayerInput playerInput;
+
+    [SerializeField]
+    private Material dugMaterial;
+
+    [SerializeField]
     private float speed = 5f;
 
     [SerializeField]
@@ -19,6 +25,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        // If there is no player controller stored, find the player controller component on the player
         if (!playerController)
         {
             playerController = this.gameObject.GetComponent<CharacterController>();
@@ -42,8 +49,51 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// Calculates movement using player input, using new input system
     /// </summary>
+    /// <param name="value"></param>
     private void OnMovement(InputValue value)
     {
         velocity = new Vector3(value.Get<Vector2>().x, 0f, value.Get<Vector2>().y);
+    }
+
+    /// <summary>
+    /// Digs a hole if the player can dig the current block they are standing on
+    /// </summary>
+    /// <param name="value"></param>
+    private void OnDig(InputValue value)
+    {
+        // Shoots a raycast out directly under the player to check for ground to dig
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity))
+        {
+            // Draws a ray for debugging
+            Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.red);
+
+            // Checks if current object has already been dug by checking the current material
+            Renderer rend = hit.collider.GetComponent<Renderer>();
+            if (rend.sharedMaterial.name != dugMaterial.name)
+            {
+                // Set current material to the dug material and start the dig action coroutine
+                rend.sharedMaterial = dugMaterial;
+                StartCoroutine("DigAction");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Restricts player input while the player's dig animation is playing
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DigAction()
+    {
+        // Disable player input
+        playerInput.actions.Disable();
+
+        // TODO: Start dig animation when animation is imported and implemented
+
+        // Wait for dig animation to finish, currently has a placeholder for time
+        yield return new WaitForSeconds(2f);
+
+        // Enable player input
+        playerInput.actions.Enable();
     }
 }
